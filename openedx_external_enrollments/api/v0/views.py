4e646e369849db5b2d78c5b300f5c0bea74adf5e
Andrey Canon <andrey.canon@edunext.co>
2019-11-20 11:52:25 -0500

    Use jwt authentication

--- openedx_external_enrollments/api/v0/views.py
+++ openedx_external_enrollments/api/v0/views.py
@@ -1,19 +1,14 @@
 """
 This file contains the views for openedx-external-enrollments API.
 """
-import requests
-import json
 import logging
+import requests
 from collections import OrderedDict
 
 from django.conf import settings
-from django.http import JsonResponse, Http404
-from django.contrib.auth.models import User
+from django.http import JsonResponse
 from opaque_keys.edx.keys import CourseKey
 from rest_framework import permissions, status
-from rest_framework.exceptions import ValidationError
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework.views import APIView
 from rest_framework_oauth.authentication import OAuth2Authentication
 
@@ -21,22 +16,28 @@ from courseware.courses import get_course_by_id
 from openedx_external_enrollments.edxapp_wrapper.get_edx_rest_framework_extensions import (
     get_jwt_authentication,
 )
-from openedx_external_enrollments.edxapp_wrapper.get_openedx_permissions import get_staff_or_owner
 
 logger = logging.getLogger(__name__)
 
 
 class ExternalEnrollment(APIView):
 
-    authentication_classes = []
-    permissions = []
+    authentication_classes = (
+        OAuth2Authentication,
+        get_jwt_authentication(),
+    )
+    permission_classes = (permissions.IsAuthenticated,)
 
     def post(self, request):
         data = request.data
         json_response = {}
-        course_key = CourseKey.from_string(request.data['course_id'])
-        course = get_course_by_id(course_key)
-        course.other_course_settings
+        course_id = data.get("course_id")
+
+        if not course_id:
+            json_response["detail"] = "The parameter course_id has not been provided."
+            return JsonResponse(json_response, status=status.HTTP_400_BAD_REQUEST)
+
+        course = get_course_by_id(CourseKey.from_string(course_id))
         data["is_active"] = True
         data["course_run_id"] = course.other_course_settings.get("external_course_run_id")
         mode_override = course.other_course_settings.get("external_enrollment_mode_override")
@@ -55,17 +56,17 @@ class ExternalEnrollment(APIView):
     @staticmethod
     def get_auth_token():
         data = OrderedDict(
-                grant_type="client_credentials",
-                client_id=settings.EDX_ENTERPRISE_API_CLIENT_ID,
-                client_secret=settings.EDX_ENTERPRISE_API_CLIENT_SECRET,
-                token_type="jwt",
+            grant_type="client_credentials",
+            client_id=settings.EDX_ENTERPRISE_API_CLIENT_ID,
+            client_secret=settings.EDX_ENTERPRISE_API_CLIENT_SECRET,
+            token_type="jwt",
         )
         try:
             response = requests.post(settings.EDX_ENTERPRISE_API_TOKEN_URL, data=data)
             if response.ok:
                 return "{} {}".format(response.json()["token_type"], response.json()["access_token"])
         except Exception as e:
-            logging.error("Failed to get token: "+ str(e))
+            logging.error("Failed to get token: " + str(e))
 
         return None
 
